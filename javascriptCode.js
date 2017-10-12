079ebcbf735574a7c572a55ce5b9259d6828eb27
Sharon Mazor <sharonmazoruk@gmail.com>
2017-10-12 17:01:06 +0100

    alerts need to stop!

--- javascriptCode.js
+++ javascriptCode.js
@@ -1,10 +1,10 @@
-alert ("Hello, world!");
-alert("Alerts are annoying aren't they?");
+//alert ("Hello, world!");
+//alert("Alerts are annoying aren't they?");
 var course, moduleSelected;
 var test1= 0;
 if (test1<3){
   test1 = 100;
   var text="Final alert I promise! The var test1 is " + test1;
-  alert(text);
+ // alert(text);
 }
 //I'm at 02:06 on working with operators
