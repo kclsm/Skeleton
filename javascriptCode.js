b8073926dad820afe0be8ee886c515c77a271b8c
Sharon Mazor <sharonmazoruk@gmail.com>
2017-10-12 17:52:25 +0100

    checking descriptions work as i think they do

--- javascriptCode.js
+++ javascriptCode.js
@@ -13,9 +13,10 @@ var shortcut=document.combowithtext
 var descriptions=new Array()
 
 //extend this list if neccessary to accomodate more selections
-descriptions[0]="Click here for Cnet, the primer technology site on the net!"
+descriptions[0]="Module 1 information will go here etc. etc."
 descriptions[1]="Click here for CNN, one of the best sources online to get your news."
 descriptions[2]="Click here for Geocities, and receive 10 megs of free web space."
+descriptions[3]="This is looooong."
 
 
 shortcut.text.value=descriptions[shortcut.example.selectedIndex]
