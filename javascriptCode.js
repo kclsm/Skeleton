bfc4cab127159cb8935d4196271098cc88982a00
Sharon Mazor <sharon.mazor@kcl.ac.uk>
2017-10-19 16:25:37 +0100

    trying out with a click

--- javascriptCode.js
+++ javascriptCode.js
@@ -13,7 +13,7 @@ var shortcut=document.combowithtext
 var descriptions=new Array()
 
 //extend this list if neccessary to accomodate more selections
-descriptions[0]="6CCS3AIN- "
+descriptions[0]="6CCS3AIN- argh!!!!!"
 descriptions[1]="Click here for CNN, one of the best sources online to get your news."
 descriptions[2]="Click here for Geocities, and receive 10 megs of free web space."
 descriptions[3]="This is looooong."
