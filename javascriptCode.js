6e26c057d7cdd28b7c3de28878fd2a57344cce95
Sharon Mazor <sharon.mazor@kcl.ac.uk>
2017-10-19 18:01:20 +0100

    ?

--- javascriptCode.js
+++ javascriptCode.js
@@ -45,13 +45,13 @@ function showtext(){
 
 function addSem2(){
 	
-	shortcutSelectionText.selectionText.value= "[shortcutsem2.sem2.selectedIndex]"
+	shortcutSelectionText.selectionText.value= sem2Description[shortcutsem2.sem2.selectedIndex]
 	addRow()
 
 }
 function addSem1(){
 	
-	shortcutSelectionText.selectionText.value="[shortcutsem1.sem1.selectedIndex]"
+	shortcutSelectionText.selectionText.value=sem1Description[shortcutsem1.sem1.selectedIndex]
 	addRow()
 
 }
