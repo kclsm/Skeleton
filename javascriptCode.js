d5aa4935270cf8bf2c6b557b469ef5b9ab5f6791
MiriamTamara <32278020+MiriamTamara@users.noreply.github.com>
2017-10-26 17:31:39 +0100

    Update javascriptCode.js
    Added some arrays to be used by table.

--- javascriptCode.js
+++ javascriptCode.js
@@ -15,7 +15,8 @@ var shortcutSelectionText=document.selectiontext
 var sem1Description=new Array()
 var sem2Description=new Array()
 var selected=new Array()
-
+var moduleTitlesSem1 = new Array()
+var moduleTitlesSem2 = new Array()
 
 //extend this list if neccessary to accomodate more selections
 
@@ -61,6 +62,45 @@ sem1Description[19]=""
 
 sem2Description[0]="test worked?"
 sem2Description[1]="yes?"	
+
+//Titles of all first semester modules. To be used by table.
+moduleTitlesSem1[0] = "Artificial Intelligence"
+moduleTitlesSem1[1] = "Artificial Intelligence Planning"
+moduleTitlesSem1[2] = "Agents and Multi-Agent Systems"
+moduleTitlesSem1[3] = "Compilers and Formal Languages"
+moduleTitlesSem1[4] = "Cryptography and Information Security"
+moduleTitlesSem1[5] = "Communication Systems"
+moduleTitlesSem1[6] = "Computer Vision"
+moduleTitlesSem1[7] = "Computer Science Logic"
+moduleTitlesSem1[8] = "Electronic Engineering Individual Project"
+moduleTitlesSem1[9] = "Group Project"
+moduleTitlesSem1[10] = "Electronic Circuits"
+moduleTitlesSem1[11] = "Computer Graphics System"
+moduleTitelsSem1[12] = "Human-Computer Interaction"
+moduleTitelsSem1[13] = "Internet Systems"
+moduleTitelsSem1[14] = "Paralell Algorithms"
+moduleTitelsSem1[15] = "Individual Project"
+moduleTitelsSem1[16] = "Sensors and Actuators"
+moduleTitlesSem1[17] = "Software Measurement and Testing"
+moduleTitlesSem1[18] = "Formal Verification"
+moduleTitlesSem1[19] = "Algorithms for the World Wide Web and Social Networks"
+
+//Titles of all second semester modules. To be used by table.
+moduleTitlesSem2[0] = "Biologically Inspired Methods"
+moduleTitlesSem2[1] = "Computational Models"
+moduleTitlesSem2[2] = "Distributed Systems"
+moduleTitlesSem2[3] = "Electronic Engineering Individual Project"
+moduleTitlesSem2[4] = "Group Project"
+moduleTitlesSem2[5] = "Hardware Design"
+moduleTitlesSem2[6] = "Network Security"
+moduleTitlesSem2[7] = "Optimisation Methods"
+moduleTitlesSem2[8] = "Pattern Recognition"
+moduleTitlesSem2[9] = "Individual Project"
+moduleTitlesSem2[10] = "Robotic Systems"
+moduleTitlesSem2[11] = "Real Time Systems and Control"
+moduleTitlesSem2[12] = "Software Architecture and Design"
+moduleTitlesSem2[13] = "Software Engineering og Internet Applications"
+moduleTitlesSem2[14] = "Text Searching and Processing"
 
 shortcutsem2.sem2text.value=sem2Description[shortcutsem2.sem2.selectedIndex]
 shortcutsem1.sem1text.value=sem1Description[shortcutsem1.sem1.selectedIndex]
