15a1f371f9b71a7ebf96968631b4fee84a8e5715
MiriamTamara <32278020+MiriamTamara@users.noreply.github.com>
2017-10-26 17:33:18 +0100

    Update javascriptCode.js
    Deleting some wierd comments.

--- javascriptCode.js
+++ javascriptCode.js
@@ -1,14 +1,3 @@
-//alert ("Hello, world!");
-//alert("Alerts are annoying aren't they?");
-/*var course, moduleSelected;
-var test1= 0;
-if (test1<3){
-  test1 = 100;
-  var text="Final alert I promise! The var test1 is " + test1;
- // alert(text);
-}
-//I'm at 02:06 on working with operators
-*/
 var shortcutsem2=document.combowithsem2text
 var shortcutsem1=document.combowithsem1text
 var shortcutSelectionText=document.selectiontext
